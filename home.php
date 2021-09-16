06ef1bc9939fa1740eb9b65a80b1dd07c1aed4cf
Chapa <gui_2512@hotmail.com>
2021-09-15 23:37:46 -0300

    Correção do tamanho dos cards no desktop

--- home.php
+++ home.php
@@ -12,7 +12,7 @@
 
     <section class="content-separator">
         <div class="container">
-            <div class="row">
+            <div class="row my-4">
                 <div class="col-12 col-md-6 scene scene--card">
                     <div class="myCard" onclick="flipCard(this);">
                         <div class="card__face card__face--front">
@@ -23,6 +23,64 @@
                             </div>
                             <div class="imgMyCard">
                                 <img class="w-100 img-fluid" src="https://static.wixstatic.com/media/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.png/v1/crop/x_0,y_0,w_498,h_302/fill/w_224,h_136,al_c,q_85,usm_0.66_1.00_0.01/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.webp" alt="">
+                            </div>
+                        </div>
+                        <div class="card__face card__face--back">
+                            <div class="textMyCard textMissao">
+                                <h4>Missão</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                            <div class="textMyCard textVisao">
+                                <h4>Visão</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                            <div class="textMyCard textVisao">
+                                <h4>Valores</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 scene scene--card">
+                    <div class="myCard" onclick="flipCard(this);">
+                        <div class="card__face card__face--front">
+                            <div class="imgMyCard">
+                                <img class="w-100 img-fluid" src="https://static.wixstatic.com/media/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.png/v1/crop/x_0,y_0,w_498,h_302/fill/w_224,h_136,al_c,q_85,usm_0.66_1.00_0.01/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.webp" alt="">
+                            </div>
+                            <div class="textMyCard">
+                                <h2>Instituto Alimentar</h2>
+                                <h5>Campanha Arrecadação de Cestas Básicas e Kits Higiene</h5>
+                                <p>Em meio à pandemia da Covid-19, o Instituto Alimentar, desenvolve a campanha de arrecadação de alimentos, cestas básicas e kits de higiene, para atender ás famílias atendidas no projeto alimentando vidas e casos pontuais que chegam a nossa instituição. Participe!</p>
+                            </div>
+                        </div>
+                        <div class="card__face card__face--back">
+                            <div class="textMyCard textMissao">
+                                <h4>Missão</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                            <div class="textMyCard textVisao">
+                                <h4>Visão</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                            <div class="textMyCard textVisao">
+                                <h4>Valores</h4>
+                                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi unde reprehenderit voluptatem dolorem facere sequi suscipit animi voluptate dolor vero porro, cum deleniti ex in saepe, necessitatibus assumenda adipisci laboriosam?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-4">
+                <div class="col-12 col-md-6 scene scene--card">
+                    <div class="myCard" onclick="flipCard(this);">
+                        <div class="card__face card__face--front">
+                            <div class="imgMyCard">
+                                <img class="w-100 img-fluid" src="https://static.wixstatic.com/media/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.png/v1/crop/x_0,y_0,w_498,h_302/fill/w_224,h_136,al_c,q_85,usm_0.66_1.00_0.01/bae5f5_1e4d803b8de34ac5996e80c0c88ca5fc~mv2.webp" alt="">
+                            </div>
+                            <div class="textMyCard">
+                                <h2>Instituto Alimentar</h2>
+                                <h5>Campanha Arrecadação de Cestas Básicas e Kits Higiene</h5>
+                                <p>Em meio à pandemia da Covid-19, o Instituto Alimentar, desenvolve a campanha de arrecadação de alimentos, cestas básicas e kits de higiene, para atender ás famílias atendidas no projeto alimentando vidas e casos pontuais que chegam a nossa instituição. Participe!</p>
                             </div>
                         </div>
                         <div class="card__face card__face--back">
